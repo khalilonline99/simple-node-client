54eb314bf7c63021e98d280678c202738536b39e
khalil <khalilonline99@gmail.com>
2022-11-15 18:10:54 +0600

    added from office

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState([])
+
+  useEffect( () => {
+    fetch('http://localhost:5000/users')
+    .then (res => res.json())
+    .then(data => setUser(data))
+  } ,[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Total User:{user.length}</h3>    
     </div>
   );
 }
